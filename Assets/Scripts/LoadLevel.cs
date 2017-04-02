cb8e3ea765861033f8be9e7fe9123489323d384f
klinger2709 <Julian@Julians-iMac.fritz.box>
2017-04-02 22:37:24 +0200

    init
    init

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+
+	public GameObject start;
+	public GameObject end;
+	public GameObject platform;
+	public GameObject hole;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void initCourtForPlan(string plan) {
+		
+	}
+}
